d816cb40758fb87ec4cc39ecc5180d758eba9446
Sykes5 <191witb23@o365.htl-leoben.at>
2023-03-08 14:36:13 +0100

    Initial commit

---
+++ src/main/java/Input_Validation.java
@@ -0,0 +1,9 @@
+public class Input_Validation {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+
+    }
+
+
+
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,11 @@
+public class User {
+
+    public User(){
+
+    }
+
+
+
+
+
+}
