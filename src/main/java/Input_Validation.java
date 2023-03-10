7a75a6a6932841968d22f1e650574650e8550253
Sykes5 <191witb23@o365.htl-leoben.at>
2023-03-10 15:00:22 +0100

    Woring CheckName Non Working checkDate

--- src/main/java/Input_Validation.java
+++ src/main/java/Input_Validation.java
@@ -1,8 +1,70 @@
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Input_Validation {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your Firstname:");
+        String firstName = sc.nextLine();
+        System.out.println("Enter your Lastname:");
+        String lastName = sc.nextLine();
+        System.out.println("Enter your Birthdate:");
+        LocalDate date = LocalDate.parse(sc.nextLine());
+
+
+        User user = new User(firstName,lastName,date,0,0,null,null);
+
+        //System.out.println(checkName(user));
+        System.out.println();
+
 
     }
+
+    public static boolean checkName(User user) {
+
+        if(user.getFirstName().length() > 64|| user.getlastName().length() > 64){
+            return false;
+        }
+
+        if ( user.getFirstName()== null || user.getlastName()== null) {
+            return false;
+        }
+        int len = user.getFirstName().length();
+        int len2 = user.getlastName().length();
+
+        for (int i = 0; i < len; i++) {
+            if ((Character.isLetter(user.getFirstName().charAt(i)) == false)) {
+                return false;
+            }
+            return true;
+        }
+        for (int i = 0; i < len2; i++) {
+            if ((Character.isLetter(user.getlastName().charAt(i)) == false)) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkDate(User user){
+        if(user.getBirthDay().isDate(date, datePattern, false)){
+            return true;
+        }
+        return false;
+
+
+    }
+
+    public static boolean isDate(String value,String datePattern, boolean strict)
+
+
 
 
 
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,11 +1,84 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public class User {
 
-    public User(){
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDay;
+    private int zipCode;
+    private int phoneNumber;
+    private String password;
+    private String reenter;
+
+
+    public User(String firstName, String lastName, LocalDate birthDay, int zipCode, int phoneNumber, String password, String reenter) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.reenter = reenter;
+    }
+
+    public User() {
 
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getlastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        lastName = lastName;
+    }
 
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getReenter() {
+        return reenter;
+    }
+
+    public void setReenter(String reenter) {
+        this.reenter = reenter;
+    }
 }
